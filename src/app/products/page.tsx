d77e6ddce4886ede29336c133a13e9447764f625
Rimantas Kabaila <rimas.kabaila@gmail.com>
2023-08-18 13:02:06 +0300

    form

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,15 @@
 const Page = () => (
-  <div className="h-screen flex flex-col justify-center items-center">
+  <div className="h-screen flex flex-col items-center">
     <div className="h-auto">Products</div>
+    <form action="/api/form" method="post">
+      <label htmlFor="first">First Name</label>
+      <input type="text" id="first" name="first" required />
+
+      <label htmlFor="last">Last Name</label>
+      <input type="text" id="last" name="last" required />
+
+      <button type="submit">Submit</button>
+    </form>
   </div>
 );
 
