23377935af10b59962b119484433f4c4093f8bd3
Rimantas Kabaila <rimas.kabaila@gmail.com>
2023-08-18 12:49:40 +0300

    fixed lint error

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,7 @@
-export default () => (
+const Page = () => (
   <div className="h-screen flex flex-col justify-center items-center">
     <div className="h-auto">Products</div>
   </div>
 );
+
+export default Page;
